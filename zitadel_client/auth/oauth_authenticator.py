99e9e096a80ab5d85b188fdedd8ce87144d6e57d
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-04-14 11:45:06 +0300

    Added tests to ensure that the JWT token based authentication is working as expected

--- zitadel_client/auth/oauth_authenticator.py
+++ zitadel_client/auth/oauth_authenticator.py
@@ -9,64 +9,65 @@ from zitadel_client.auth.open_id import OpenId
 
 
 class OAuthAuthenticator(Authenticator, ABC):
-  """
-  Base class for OAuth-based authentication using Authlib.
-
-  Attributes:
-      open_id: An object providing OAuth endpoint information.
-      oauth_session: An OAuth2Session instance used for fetching tokens.
-  """
-
-  def __init__(self, open_id: OpenId, oauth_session: OAuth2Session):
     """
-    Constructs an OAuthAuthenticator.
+    Base class for OAuth-based authentication using Authlib.
 
-    :param open_id: An object that must implement get_host_endpoint() and get_token_endpoint().
-    :param oauth_session: The scope for the token request.
+    Attributes:
+        open_id: An object providing OAuth endpoint information.
+        oauth_session: An OAuth2Session instance used for fetching tokens.
     """
-    super().__init__(open_id.get_host_endpoint())
-    self.open_id = open_id
-    self.token: Optional[Token] = None
-    self.oauth_session = oauth_session
 
-  def get_auth_token(self) -> str:
-    """
-    Returns the current access token, refreshing it if necessary.
-    """
-    if self.token is None or self.token.is_expired():
-      self.refresh_token()
-    return self.token.access_token
+    def __init__(self, open_id: OpenId, oauth_session: OAuth2Session):
+        """
+        Constructs an OAuthAuthenticator.
 
-  def get_auth_headers(self) -> Dict[str, str]:
-    """
-    Retrieves authentication headers.
+        :param open_id: An object that must implement get_host_endpoint() and get_token_endpoint().
+        :param oauth_session: The scope for the token request.
+        """
+        super().__init__(open_id.get_host_endpoint())
+        self.open_id = open_id
+        self.token: Optional[Token] = None
+        self.oauth_session = oauth_session
 
-    :return: A dictionary containing the 'Authorization' header.
-    """
-    return {"Authorization": "Bearer " + self.get_auth_token()}
+    def get_auth_token(self) -> str:
+        """
+        Returns the current access token, refreshing it if necessary.
+        """
+        if self.token is None or self.token.is_expired():
+            self.refresh_token()
+        return self.token.access_token
 
-  @abstractmethod
-  def get_grant(self) -> dict:
-    """
-    Builds and returns a dictionary of grant parameters required for the token request.
+    def get_auth_headers(self) -> Dict[str, str]:
+        """
+        Retrieves authentication headers.
 
-    For example, this might include parameters like grant_type, client_assertion, etc.
-    """
-    pass
+        :return: A dictionary containing the 'Authorization' header.
+        """
+        return {"Authorization": "Bearer " + self.get_auth_token()}
 
-  def refresh_token(self) -> Token:
-    """
-    Refreshes the access token using the OAuth flow.
-    It uses get_grant() to obtain all necessary parameters, such as the grant type and any assertions.
+    @abstractmethod
+    def get_grant(self) -> dict:
+        """
+        Builds and returns a dictionary of grant parameters required for the token request.
 
-    :return: A new Token.
-    """
-    try:
-      token_response = self.oauth_session.fetch_token(url=(self.open_id.get_token_endpoint()), **(self.get_grant()))
-      access_token = token_response["access_token"]
-      expires_in = token_response.get("expires_in", 3600)
-      expires_at = datetime.now(timezone.utc) + timedelta(seconds=expires_in)
-      self.token = Token(access_token, expires_at)
-      return self.token
-    except Exception as e:
-      raise Exception("Failed to refresh token: " + str(e)) from e
+        For example, this might include parameters like grant_type, client_assertion, etc.
+        """
+        pass
+
+    def refresh_token(self) -> Token:
+        """
+        Refreshes the access token using the OAuth flow.
+        It uses get_grant() to obtain all necessary parameters, such as the grant type and any assertions.
+
+        :return: A new Token.
+        """
+        try:
+            token_response = self.oauth_session.fetch_token(url=(self.open_id.get_token_endpoint()),
+                                                            **(self.get_grant()))
+            access_token = token_response["access_token"]
+            expires_in = token_response.get("expires_in", 3600)
+            expires_at = datetime.now(timezone.utc) + timedelta(seconds=expires_in)
+            self.token = Token(access_token, expires_at)
+            return self.token
+        except Exception as e:
+            raise Exception("Failed to refresh token: " + str(e)) from e
--- zitadel_client/auth/personal_access_token_authenticator.py
+++ zitadel_client/auth/personal_access_token_authenticator.py
@@ -5,20 +5,20 @@ from zitadel_client.utils.url_util import URLUtil
 
 
 class PersonalAccessTokenAuthenticator(Authenticator):
-  """
-  Personal Access Token Authenticator.
-
-  Uses a static personal access token for API authentication.
-  """
-
-  def __init__(self, host: str, token: str):
-    super().__init__(URLUtil.build_hostname(host))
-    self.token = token
-
-  def get_auth_headers(self) -> Dict[str, str]:
     """
-    Returns the authentication headers using the personal access token.
+    Personal Access Token Authenticator.
 
-    :return: A dictionary containing the 'Authorization' header.
+    Uses a static personal access token for API authentication.
     """
-    return {"Authorization": "Bearer " + self.token}
+
+    def __init__(self, host: str, token: str):
+        super().__init__(URLUtil.build_hostname(host))
+        self.token = token
+
+    def get_auth_headers(self) -> Dict[str, str]:
+        """
+        Returns the authentication headers using the personal access token.
+
+        :return: A dictionary containing the 'Authorization' header.
+        """
+        return {"Authorization": "Bearer " + self.token}
